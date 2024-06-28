97c6cb381486c6128ce501a0ee2b6d2d0c52de62
YashDxr <yashdxr@gmail.com>
2024-06-28 16:44:36 +0530

    second3

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const connectDB = require('./config/db.js');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
+const express = require("express");
+const connectDB = require("./config/db.js");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.use('/api/users', require('./routes/users.js'));
-app.use('/api/discussions', require('./routes/discussions.js'));
+app.use("/api/users", require("./routes/users.js"));
+app.use("/api/discussions", require("./routes/discussions.js"));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => {
+  res.redirect("/api-docs");
+});
 
 const PORT = process.env.PORT || 5000;
 
